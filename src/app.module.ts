931b5cb0e364ad765f09e1fffd0f49e69982f65e
ClydeRM <clyde.official@icloud.com>
2022-05-10 16:05:28 +0800

    feat(Module): Import ConfigModule
    Load app.config/appConfig object

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { PrismaModule } from './prisma/prisma.module';
 
+import appConfig from './config/app.config';
+
 @Module({
-  imports: [AuthModule, UserModule, BookmarksModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ load: [appConfig] }),
+    AuthModule,
+    UserModule,
+    BookmarksModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
