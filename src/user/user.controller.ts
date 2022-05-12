31bb7a946d9999335343c9c7bfb44054b3578880
ClydeRM <clyde.official@icloud.com>
2022-05-12 18:07:42 +0800

    feat(Controller): Setup /users route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
+
+import { UserService } from './user.service';
+
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+
+import { EditUserDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Get('me')
   getme(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editme(@GetUser('id') userId: string, @Body() editUserDto: EditUserDto) {
+    return this.userService.editUser(userId, editUserDto);
   }
 }
